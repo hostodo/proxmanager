fc000883ddad34f5801b6ac8f6ec78978a641b71
hbazzi <me@hassanb.com>
2023-10-19 01:41:34 -0400

    fix

--- install/install.sh
+++ install/install.sh
@@ -3,5 +3,5 @@
 adduser proxmanager
 cp ./config/proxmanager-py.service /etc/systemd/system/proxmanager-py.service
 
-systemctl start proxmanager
-systemctl enable proxmanager
+systemctl start proxmanager-py
+systemctl enable proxmanager-py
