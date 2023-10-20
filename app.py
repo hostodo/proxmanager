8f9159e1b7d16840dddab94bccd6799c7c9f48a6
hbazzi <me@hassanb.com>
2023-10-20 00:21:02 -0400

    routes

--- app.py
+++ app.py
@@ -106,15 +106,13 @@ def snippets_network_vmid_post(vm_id):
                 "gateway6": ipv6_addresses[0].get('gateway'),
                 "routes": [
                     {
-                        "to": ipv6_addresses[0].get('gateway')
-                    },
-                    {
-                        "to": "66.187.7.0/24",
-                        "via": "66.187.7.1"
-                    },
-                    {
-                        "to": "::/0",
+                        "to": "default",
+                        "on-link": True,
                         "via": ipv6_addresses[0].get('gateway')
+                    },
+                    {
+                        "to": "default",
+                        "via": "66.187.7.1"
                     }
                 ],
                 "nameservers": {
