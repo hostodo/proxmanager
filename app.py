838378332d015f4042920e5001c58f3fda91b45f
hbazzi <me@hassanb.com>
2023-10-20 23:20:21 -0400

    switch back to netplan v2

--- app.py
+++ app.py
@@ -130,11 +130,11 @@ def snippets_network_vmid_post(vm_id):
         yaml = YAML()
         yaml.preserve_quotes = True
         yaml.explicit_start = True
-        yaml.dump(cloud_init_network, outfile)
+        yaml.dump(cloud_init_network_v2, outfile)
 
     return {
         "file_path": config_file_path,
-        "config": cloud_init_network
+        "config": cloud_init_network_v2
     }
 
 
